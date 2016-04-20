375b5f5d8bd8510df64afc089450d8a83a85812b
张傲天 <zhangaotian@zhangaotiandemacbook-pro.local>
2016-04-20 18:50:45 +0800

    Initial Commit

---
+++ alibabaExam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alibabaExam
+//
+//  Created by 张傲天 on 16/4/20.
+//  Copyright © 2016年 张傲天. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
