59bbb3483b8f5d3aa0287d47e8e023e5d56c25f7
babyfucker <1193884326@qq.com>
2017-02-07 03:49:06 +0800

    20170207

--- leetCode13/leetCode13/main.cpp
+++ leetCode13/leetCode13/main.cpp
@@ -9,50 +9,102 @@
 #include <iostream>
 #include <math.h>
 #include <sstream>
+#include <vector>
 using namespace std;
 class Solution {
 public:
-    string decodeString(string s) {
-        if (s.length() < 2) {
-            return s;
-        }
-        int i = (int)s.length() - 1;//left
-        int j = 0;//right
-        for (i = (int)s.length() - 1; i > 0; i--) {
-            if (s[i] == '[') {
-                j = i + 1;
-                while (s[j] != ']') {
-                    j++;
-                }
-                string inside = decodeString(s.substr(i + 1,j - i - 1));
-                string answer;
-                int times = 0;
-                int h = i - 1;
-                int weishu = 0;
-                while (s[h] <= '9' && h >= 0) {
-                    h--;
-                    weishu++;
-                }
-                times = atoi(s.substr(h + 1,weishu).data());
-                for (int k = 0; k < times; k++) {
-                    answer = answer + inside;
-                }
-                s.replace(i - weishu, j - i + weishu + 1,answer);
+//    string decodeString(string s) {
+//        int i = 0;//left
+//        int j = 0;//right
+//        vector<int> l;
+//        vector<int> r;
+//        vector<int> lborder;
+//        vector<int> rborder;
+//        for (int z = 0; z < s.length(); z++) {
+//            if (s[z] == '[') {
+//                l.push_back(z);
+//            }
+//            if (s[z] == ']') {
+//                r.push_back(z);
+//            }
+//        }
+//        if (l.size() == 0) {
+//            return s;
+//        }
+//        if (l.size() == 1) {
+//            lborder.push_back(l[0]);
+//            rborder.push_back(r[0]);
+//        } else {
+//            lborder.push_back(l[0]);
+//            int xinhaoliang = 0;
+//            for (int n = 0; n < l.size() - 1; n++) {
+//                if (l[n + 1] > r[n]) {
+//                    if (xinhaoliang == 0) {
+//                        rborder.push_back(r[n]);
+//                        xinhaoliang = 1;
+//                    }
+//                    lborder.push_back(l[n + 1]);
+//                    rborder.push_back(r[n + 1]);
+//                }
+//            }
+//            if (xinhaoliang == 0) {
+//                rborder.push_back(r[r.size() - 1]);
+//                xinhaoliang = 1;
+//            }
+//        }
+//        int cha = 0;
+//        for (int m = 0; m < (int)lborder.size(); m++) {
+//            i = lborder[m] - cha;
+//            j = rborder[m] - cha;
+//            string inside = decodeString(s.substr(i + 1,j - i - 1));
+//            string answer;
+//            int times = 0;
+//            int h = i - 1;
+//            int weishu = 0;
+//            while (s[h] <= '9' && h >= 0) {
+//                h--;
+//                weishu++;
+//            }
+//            times = atoi(s.substr(h + 1,weishu).data());
+//            for (int k = 0; k < times; k++) {
+//                answer = answer + inside;
+//            }
+//            s.replace(i - weishu, j - i + weishu + 1,answer);
+//            cha = cha + j - i + weishu + 1- (int)answer.size();
+//        }
+//        return s;
+//    }
+    string decodeString(string s, int &i) {
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i]))
+                res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                
+                i++; // '['
+                string t = decodeString(s, i);
+                i++; // ']'
+                
+                while (n-- > 0)
+                    res += t;
             }
         }
-        return s;
+        
+        return res;
+    }
+    string decodeString(string s) {
+        int i = 0;
+        return decodeString(s, i);
     }
 };
 int main() {
     // insert code here...
     std::cout << "Hello, World!\n";
-//    int a = '9';
-//    char b = 96;
-//    cout<<a<<endl;
     Solution *s = new Solution;
-    cout<<s->decodeString("11[a]");
-//    string a = "0123456789";
-//    a.replace(3, 4, "asd");
-//    cout<<a<<endl;
+    cout<<s->decodeString("10[b]4[ch]")<<endl;
     return 0;
 }
